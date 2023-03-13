2d3c1237f8eb825e4f9a2e70cc6b207571f4a150
PetrAlexkulakov <petr.alex.kulakov@gmail.com>
2023-03-13 23:18:47 +0300

    fix: fix configs

--- react-project/src/App.tsx
+++ react-project/src/App.tsx
@@ -1,6 +1,7 @@
 import { useState } from 'react';
-import reactLogo from './assets/react.svg';
+//import reactLogo from './assets/react.svg';
 import './App.css';
+import React from 'react';
 
 function App() {
   const [count, setCount] = useState(0);
@@ -8,11 +9,8 @@ function App() {
   return (
     <div className="App">
       <div>
-        <a href="https://vitejs.dev" target="_blank">
+        <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
           <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
       <h1>Vite + React</h1>
