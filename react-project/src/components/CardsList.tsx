c213e4b5527415bd5213306c1b175392eef6d8bc
PetrAlexkulakov <petr.alex.kulakov@gmail.com>
2023-03-19 12:46:06 +0300

    fix: fix build command

--- react-project/src/components/CardsList.tsx
+++ react-project/src/components/CardsList.tsx
@@ -3,10 +3,17 @@ import CardItem from './CardItem';
 import axios from 'axios';
 import MyInput from './UI/input/MyInput';
 
-const posts: { id: number; title: string; body: string }[] = (
-  await axios.get('https://jsonplaceholder.typicode.com/posts')
-).data.slice(0, 10);
-const images = (await axios.get('https://jsonplaceholder.typicode.com/photos')).data.slice(0, 20);
+let posts: { id: number; title: string; body: string }[];
+let images: { url: string }[];
+
+(async function doPosts() {
+  posts = (await axios.get('https://jsonplaceholder.typicode.com/posts')).data.slice(0, 10);
+  images = (await axios.get('https://jsonplaceholder.typicode.com/photos')).data.slice(0, 20);
+})();
+// const posts: { id: number; title: string; body: string }[] = (
+//   await axios.get('https://jsonplaceholder.typicode.com/posts')
+// ).data.slice(0, 10);
+// const images = (await axios.get('https://jsonplaceholder.typicode.com/photos')).data.slice(0, 20);
 
 class CardsList extends React.Component {
   private count: number;
