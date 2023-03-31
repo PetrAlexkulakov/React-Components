746152ede702cc0b45c7f6d831d70dce0745464c
PetrAlexkulakov <petr.alex.kulakov@gmail.com>
2023-03-31 19:14:50 +0300

    refactor: change forms to useRef

--- react-project/src/pages/Forms.tsx
+++ react-project/src/pages/Forms.tsx
@@ -1,4 +1,4 @@
-import React, { ChangeEventHandler, MouseEventHandler, RefObject } from 'react';
+import React, { ChangeEventHandler, MouseEventHandler, useState, useRef } from 'react';
 import Header from '../components/Header';
 import MyButton from '../components/UI/button/MyButton';
 import MyInput from '../components/UI/input/MyInput';
@@ -6,74 +6,172 @@ import CardItem from '../components/CardItem';
 import { BaseSyntheticEvent } from 'react';
 
 type MyState = {
-  posts: {
-    body?: string;
-    title?: string;
-    key?: string;
-    city?: string;
-    accept?: string;
-    switch?: string;
-    image?: string;
-  }[];
-};
+  body?: string;
+  title?: string;
+  key?: string;
+  city?: string;
+  accept?: string;
+  switch?: string;
+  image?: string;
+}[];
 
-class Forms extends React.Component<Record<string, never>, MyState> {
-  private form: HTMLFormElement | null;
-  private formName: RefObject<HTMLInputElement>;
-  private formDate: RefObject<HTMLInputElement>;
-  private formCity: string;
-  private formAccept: RefObject<HTMLInputElement>;
-  private formSwitch: string;
-  private formFile: RefObject<HTMLInputElement>;
-  constructor(props: Record<string, never>) {
-    super(props);
-    this.state = { posts: [] };
+// class Forms extends React.Component<Record<string, never>, MyState> {
+//   private form: HTMLFormElement | null;
+//   private formName: RefObject<HTMLInputElement>;
+//   private formDate: RefObject<HTMLInputElement>;
+//   private formCity: string;
+//   private formAccept: RefObject<HTMLInputElement>;
+//   private formSwitch: string;
+//   private formFile: RefObject<HTMLInputElement>;
+//   constructor(props: Record<string, never>) {
+//     super(props);
+//     this.state = { posts: [] };
 
-    this.form = null;
-    this.formName = React.createRef() as RefObject<HTMLInputElement>;
-    this.formDate = React.createRef() as RefObject<HTMLInputElement>;
-    this.formCity = 'Moscow';
-    this.selectChange = this.selectChange.bind(this);
-    this.formAccept = React.createRef() as RefObject<HTMLInputElement>;
-    this.formSwitch = 'First';
-    this.formFile = React.createRef() as RefObject<HTMLInputElement>;
-  }
+//     this.form = null;
+//     this.formName = React.createRef() as RefObject<HTMLInputElement>;
+//     this.formDate = React.createRef() as RefObject<HTMLInputElement>;
+//     this.formCity = 'Moscow';
+//     this.selectChange = this.selectChange.bind(this);
+//     this.formAccept = React.createRef() as RefObject<HTMLInputElement>;
+//     this.formSwitch = 'First';
+//     this.formFile = React.createRef() as RefObject<HTMLInputElement>;
+//   }
 
-  private addNewCard = (e: MouseEvent) => {
+//   private addNewCard = (e: MouseEvent) => {
+//     e.preventDefault();
+//     if (this.validForm() && confirm('Are u sure?')) {
+//       this.setState({
+//         posts: [
+//           ...this.state.posts,
+//           {
+//             key: String(new Date()),
+//             title: this.formDate.current?.value,
+//             body: this.formName.current?.value,
+//             city: this.formCity,
+//             switch: this.formSwitch,
+//             image: URL.createObjectURL(this.formFile.current!.files![0]),
+//           },
+//         ],
+//       });
+//       this.formSwitch = 'First';
+//       this.clearForm();
+//     }
+//   };
+
+//   private clearForm() {
+//     this.form?.reset();
+//   }
+
+//   private selectChange(e: BaseSyntheticEvent) {
+//     this.formCity = String(e.target.value);
+//   }
+
+//   private validForm() {
+//     if (
+//       this.formDate.current?.value === '' ||
+//       this.formName.current?.value === '' ||
+//       this.formFile.current!.files![0] == null ||
+//       this.formAccept.current?.checked === false
+//     ) {
+//       alert('Check the form fields!');
+//       return false;
+//     }
+//     return true;
+//   }
+
+//   render() {
+//     return (
+//       <div>
+//         <Header title="Forms" />
+//         <form className="form-wrapper" ref={(form) => (this.form = form)}>
+//           <div>
+//             Text: <MyInput ref={this.formName} type="text" placeholder="Text" />
+//           </div>
+//           <div>
+//             Date: <MyInput ref={this.formDate} type="date" placeholder="Date" />
+//           </div>
+//           <div>
+//             City:
+//             <select
+//               onChange={this.selectChange as unknown as ChangeEventHandler<HTMLSelectElement>}
+//             >
+//               <option value="Moscow">Moscow</option>
+//               <option value="New York">New York</option>
+//               <option value="Another">Another</option>
+//             </select>
+//           </div>
+//           <div>
+//             <MyInput ref={this.formAccept} type="checkbox" /> I consent to my personal data
+//           </div>
+//           <div>
+//             <MyInput
+//               onClick={() => (this.formSwitch = 'First')}
+//               type="radio"
+//               name="number"
+//               defaultChecked
+//             />{' '}
+//             First{' '}
+//             <MyInput onClick={() => (this.formSwitch = 'Second')} type="radio" name="number" />{' '}
+//             Second
+//           </div>
+//           <MyInput ref={this.formFile} type="file" />
+//           <MyButton onClick={this.addNewCard as unknown as MouseEventHandler<HTMLButtonElement>}>
+//             Create Card
+//           </MyButton>
+//         </form>
+//         <div className="card-list">
+//           {this.state.posts.map((post) => (
+//             <CardItem {...post} image={post.image} key={post.key} />
+//           ))}
+//         </div>
+//       </div>
+//     );
+//   }
+// }
+const Forms = () => {
+  const [posts, setPosts] = useState<MyState>([]);
+  let thisForm: HTMLFormElement | null = null;
+  const formName = useRef<HTMLInputElement | null>(null);
+  const formDate = useRef<HTMLInputElement | null>(null);
+  let formCity = 'Moscow';
+  // const selectChange = selectChange.bind(this);
+  const formAccept = useRef<HTMLInputElement | null>(null);
+  let formSwitch = 'First';
+  const formFile = useRef<HTMLInputElement | null>(null);
+
+  const addNewCard = (e: MouseEvent) => {
     e.preventDefault();
-    if (this.validForm() && confirm('Are u sure?')) {
-      this.setState({
-        posts: [
-          ...this.state.posts,
-          {
-            key: String(new Date()),
-            title: this.formDate.current?.value,
-            body: this.formName.current?.value,
-            city: this.formCity,
-            switch: this.formSwitch,
-            image: URL.createObjectURL(this.formFile.current!.files![0]),
-          },
-        ],
-      });
-      this.formSwitch = 'First';
-      this.clearForm();
+    if (validForm() && confirm('Are u sure?')) {
+      setPosts([
+        ...posts,
+        {
+          key: String(new Date()),
+          title: formDate.current?.value,
+          body: formName.current?.value,
+          city: formCity,
+          switch: formSwitch,
+          image: URL.createObjectURL(formFile.current!.files![0]),
+        },
+      ]);
+      formSwitch = 'First';
+      clearForm();
     }
   };
 
-  private clearForm() {
-    this.form?.reset();
+  function clearForm() {
+    thisForm?.reset();
   }
 
-  private selectChange(e: BaseSyntheticEvent) {
-    this.formCity = String(e.target.value);
+  function selectChange(e: BaseSyntheticEvent) {
+    formCity = String(e.target.value);
   }
 
-  private validForm() {
+  function validForm() {
     if (
-      this.formDate.current?.value === '' ||
-      this.formName.current?.value === '' ||
-      this.formFile.current!.files![0] == null ||
-      this.formAccept.current?.checked === false
+      formDate.current?.value === '' ||
+      formName.current?.value === '' ||
+      formFile.current!.files![0] == null ||
+      formAccept.current?.checked === false
     ) {
       alert('Check the form fields!');
       return false;
@@ -81,54 +179,49 @@ class Forms extends React.Component<Record<string, never>, MyState> {
     return true;
   }
 
-  render() {
-    return (
-      <div>
-        <Header title="Forms" />
-        <form className="form-wrapper" ref={(form) => (this.form = form)}>
-          <div>
-            Text: <MyInput ref={this.formName} type="text" placeholder="Text" />
-          </div>
-          <div>
-            Date: <MyInput ref={this.formDate} type="date" placeholder="Date" />
-          </div>
-          <div>
-            City:
-            <select
-              onChange={this.selectChange as unknown as ChangeEventHandler<HTMLSelectElement>}
-            >
-              <option value="Moscow">Moscow</option>
-              <option value="New York">New York</option>
-              <option value="Another">Another</option>
-            </select>
-          </div>
-          <div>
-            <MyInput ref={this.formAccept} type="checkbox" /> I consent to my personal data
-          </div>
-          <div>
-            <MyInput
-              onClick={() => (this.formSwitch = 'First')}
-              type="radio"
-              name="number"
-              defaultChecked
-            />{' '}
-            First{' '}
-            <MyInput onClick={() => (this.formSwitch = 'Second')} type="radio" name="number" />{' '}
-            Second
-          </div>
-          <MyInput ref={this.formFile} type="file" />
-          <MyButton onClick={this.addNewCard as unknown as MouseEventHandler<HTMLButtonElement>}>
-            Create Card
-          </MyButton>
-        </form>
-        <div className="card-list">
-          {this.state.posts.map((post) => (
-            <CardItem {...post} image={post.image} key={post.key} />
-          ))}
+  return (
+    <div>
+      <Header title="Forms" />
+      <form className="form-wrapper" ref={(form) => (thisForm = form)}>
+        <div>
+          Text: <MyInput ref={formName} type="text" placeholder="Text" />
         </div>
+        <div>
+          Date: <MyInput ref={formDate} type="date" placeholder="Date" />
+        </div>
+        <div>
+          City:
+          <select onChange={selectChange as unknown as ChangeEventHandler<HTMLSelectElement>}>
+            <option value="Moscow">Moscow</option>
+            <option value="New York">New York</option>
+            <option value="Another">Another</option>
+          </select>
+        </div>
+        <div>
+          <MyInput ref={formAccept} type="checkbox" /> I consent to my personal data
+        </div>
+        <div>
+          <MyInput
+            onClick={() => (formSwitch = 'First')}
+            type="radio"
+            name="number"
+            defaultChecked
+          />{' '}
+          First <MyInput onClick={() => (formSwitch = 'Second')} type="radio" name="number" />{' '}
+          Second
+        </div>
+        <MyInput ref={formFile} type="file" />
+        <MyButton onClick={addNewCard as unknown as MouseEventHandler<HTMLButtonElement>}>
+          Create Card
+        </MyButton>
+      </form>
+      <div className="card-list">
+        {posts.map((post) => (
+          <CardItem {...post} image={post.image} key={post.key} />
+        ))}
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
 
 export default Forms;
