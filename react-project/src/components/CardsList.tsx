0be591e62d8ab643216eca453ff5bd3ea57c1865
PetrAlexkulakov <petr.alex.kulakov@gmail.com>
2023-04-05 19:45:59 +0300

    refactor: search on api

--- react-project/src/components/CardsList.tsx
+++ react-project/src/components/CardsList.tsx
@@ -8,35 +8,40 @@ const CardsList = () => {
   const [sortedPosts, setSortedPosts] = useState([{ image: '', name: '', status: '', id: 0 }]);
   const [isLoaded, setLoaded] = useState(false);
 
-  // function handleChange(event: ChangeEvent<HTMLInputElement>) {
-  //   setDefVal(event.target.value);
-  //   localStorage.setItem('searchString', event.target.value);
-  //   setSortedPosts(
-  //     posts.filter((post) => post.title.toLowerCase().includes(event.target.value.toLowerCase()))
-  //   );
-  // }
+  function handleChange(event: ChangeEvent<HTMLInputElement>) {
+    setDefVal(event.target.value);
+    localStorage.setItem('searchString', event.target.value);
+    changeSortedPosts();
+  }
 
   useEffect(() => {
-    console.log('effect');
-    fetch('https://rickandmortyapi.com/api/character')
+    changeSortedPosts();
+  }, []);
+
+  function changeSortedPosts() {
+    fetch(`https://rickandmortyapi.com/api/character/?name=${localStorage.getItem('searchString')}`)
       .then((resp) => resp.json())
       .then((resp) => {
         setSortedPosts(resp.results);
         setLoaded(true);
       });
-  }, []);
+  }
 
-  console.log('render');
   return (
     <div>
-      {/* <SearchForm data-testid="search-input" def={defValue} onChange={handleChange} /> */}
+      <SearchForm data-testid="search-input" def={defValue} onChange={handleChange} />
       <div>
         {!isLoaded ? (
           <div>Loading...</div>
         ) : (
           <div className="card-list">
             {sortedPosts?.map((post) => (
-              <CardItem image={post.image} title={post.name} body={post.status} key={post.id} />
+              <CardItem
+                image={post.image}
+                title={post.name}
+                body={`Status: ${post.status}`}
+                key={post.id}
+              />
             ))}
           </div>
         )}
